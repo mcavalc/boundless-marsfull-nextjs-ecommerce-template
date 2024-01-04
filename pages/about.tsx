7ed85e6f1c0d795b55d32c22859b78790ae141c6
mcavalca <mcavalca01@gmail.com>
2024-01-04 13:29:37 -0300

    modified:   pages/about.tsx 	modified:   public/site.webmanifest

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,21 +8,19 @@ export default function ShippingPage({mainMenu, footerMenu}: IPageProps) {
 	return (
 		<MainLayout mainMenu={mainMenu} footerMenu={footerMenu}>
 			<div className={'container-xxl'}>
-				<h1 className='page-heading page-heading_h1  page-heading_m-h1'>About</h1>
+				<h1 className='page-heading page-heading_h1  page-heading_m-h1'>Sobre a Piramide Imports</h1>
 				<div className='text-container'>
 					<p>
-						Lorem ipsum dolor sit amet consectetur adipiscing elit semper penatibus purus, neque sodales fames
-						pellentesque luctus fringilla libero dis ornare cursus sociosqu, elementum commodo efficitur mauris dictum
-						tortor per ultricies a. Venenatis turpis eleifend proin a dapibus ullamcorper conubia odio vel, porta nulla
-						gravida habitasse facilisi quisque class non aliquam velit, donec nostra amet ante montes ad varius mauris.
-						Maecenas urna laoreet sociosqu tempus rutrum quis ad diam, habitant morbi augue ornare orci a suscipit faucibus,
-						felis nostra senectus at mollis et gravida. Tempus nulla nec etiam mollis, duis lacinia dictum netus,
-						dignissim mi facilisis. Euismod gravida mollis integer class convallis lacinia ad, elit id pharetra ac
-						bibendum iaculis augue lorem, purus at scelerisque posuere dictumst dis. Risus eleifend maecenas cras ac
-						conubia proin nisl, ultrices mollis sem et molestie dolor facilisis, venenatis neque eget maximus quis per.
-						Ante vehicula interdum pulvinar sodales faucibus, metus dignissim orci habitasse. Id dui in amet vehicula luctus
-						lobortis facilisi gravida dis litora diam sapien eget, tortor semper erat turpis pretium odio tempor porta
-						vestibulum mauris congue.
+					A história da Piramide Imports é uma jornada de paixão e dedicação ao comércio. Tudo começou com o pai, um pioneiro no ramo, que se dedicou a vender seus produtos no vibrante centro de São Paulo, percorrendo locais como a famosa Santa Ifigênia e diversas outras galerias pela cidade. Sua determinação e amor pelo comércio marcaram o início de uma trajetória inspiradora.
+					</p>
+					<p>
+					Hoje, a missão é continuada pelos filhos, que herdaram não apenas o negócio, mas também a paixão do pai. Eles estão levando a Piramide Imports a novos horizontes, abraçando a era digital para expandir o alcance da empresa. Com o apoio da internet, agora atendemos todo o Brasil, operando incansavelmente 24 horas por dia, 7 dias por semana.	
+					</p>
+					<p>
+					Na Piramide Imports, somos apaixonados tanto por produtos nacionais quanto importados, e temos um carinho especial pela tecnologia. Essa paixão nos motiva a ir além, garantindo que oferecemos apenas o melhor aos nossos clientes. Nosso time de especialistas é meticuloso na escolha dos produtos, sempre buscando a melhor relação custo-benefício. Para nós, qualidade e acessibilidade andam de mãos dadas.	
+					</p>
+					<p>
+					Em nossa loja, você encontrará uma gama diversificada de produtos, escolhidos a dedo por quem entende do assunto. Seja bem-vindo à Piramide Imports, onde a tradição encontra a inovação, e cada cliente é parte da nossa história contínua.	
 					</p>
 				</div>
 			</div>
